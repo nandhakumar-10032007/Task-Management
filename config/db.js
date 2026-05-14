6560f734c53fbc054ca44296dd154ffcdcbf254a
Nantha <nandhakumarr.cse2024@citchennai.net>
2026-05-14 10:45:17 +0530

    Fixed db syntax

--- config/db.js
+++ config/db.js
@@ -8,21 +8,6 @@ const connectDB = async () => {
 
         console.log('MongoDB Connected');
     } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect(
-            'mongodb+srv://NANTHA:Nanu12345@advance-research-cluste.oq60ytc.mongodb.net/task_manager_db?retryWrites=true&w=majority&appName=advance-research-cluster'
-        );
-
-        console.log('MongoDB Connected');
-    } catch (error) {
         console.error(error);
         process.exit(1);
     }
